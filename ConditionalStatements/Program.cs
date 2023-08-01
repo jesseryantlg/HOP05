3a3135f6e141430da7ae8293a8007620ebb2d7f8
jesseryantlg <121136297+jesseryantlg@users.noreply.github.com>
2023-08-01 11:16:36 -0700

    first commit

---
+++ ConditionalStatements/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int num = 1; 
+
+        Console.WriteLine("Guess a number (1 - 10): "); 
+        int userInput = Convert.ToInt32(Console.ReadLine()); //receieve user input and store in "userInput" variable
+
+        if (userInput == num)
+        {
+            Console.WriteLine("Your guess was correct!");
+        }
+        else if(userInput != num)
+        {
+            Console.WriteLine("You guessed wrong. Good luck next time.");
+        }
+    }
+}
